eabc4d801ba64ae5d93594ce6e7ab65cd2bb9b54
bilikhasan <fatherlyy@hotmail.com.tr>
2024-10-10 22:08:09 +0300

    devamı gelecek.

--- Project2WooxTravel/Context/TravelContext.cs
+++ Project2WooxTravel/Context/TravelContext.cs
@@ -12,5 +12,9 @@ namespace Project2WooxTravel.Context
         public DbSet<Category> Categories { get; set; }
         public DbSet<Contact> Contacts { get; set; }
         public DbSet<Destination> Destinations { get; set; }
+
+        public DbSet<Reservation> Reservations { get; set; }
+
+        public DbSet<Admin> Admins { get; set; }
     }
 }
